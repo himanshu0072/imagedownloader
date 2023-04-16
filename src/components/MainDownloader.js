580e24c28bae940a0acdd73b220c80d3a5bb771b
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-04-16 23:58:33 +0530

    script.js file is deleted and refactored

--- src/components/MainDownloader.js
+++ src/components/MainDownloader.js
@@ -1,14 +1,35 @@
 import React from "react";
 import "../style/style.css";
+// import "../components/Scripts";
+import { useRef } from "react";
 
+function fetchFile(url) {
+  fetch(url)
+    .then((res) => res.blob())
+    .then((file) => {
+      console.log("file");
+    });
+}
 function MainDownloader() {
+  const fileInputRef = useRef(null);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    fetchFile(fileInputRef.current.value);
+  };
+
   return (
     <>
       <div className="container">
         <div className="FormContainer">
           <h2 className="title">Download Images Here</h2>
-          <input placeholder="Paste image Link Here.."></input>
-          <button className="button">Download</button>
+          <input
+            placeholder="Paste image Link Here.."
+            ref={fileInputRef}
+          ></input>
+          <button className="button" onClick={handleClick}>
+            Download
+          </button>
           <div className="para">
             <p>contact for query : himanshu2003prajapati@gmail.com</p>
             <p>or</p>
